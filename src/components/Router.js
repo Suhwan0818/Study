ebde5f83793bd992933d11acd3c3637dd571f5e8
Suhwan0818 <suhwan.k.gbc@gmail.com>
2021-07-19 15:43:44 +0900

    clone coding

--- src/components/Router.js
+++ src/components/Router.js
@@ -1,13 +1,27 @@
 import React, { useState } from "react";
 import { HashRouter as Router, Route, Switch } from "react-router-dom";
+import Auth from "../routes/Auth"
+import Home from  "../routes/Home"
 
-export default => {
-    const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+const AppRouter = ({isLoggedIn}) => {
     return(
         <Router>
             <Switch>
-                {isLoggedIn ? show home : show login page}
+                {isLoggedIn ? (
+                    <>
+                        <Route exact path = "/">
+                            <Home />
+                        </Route>
+                    </>
+                ) : (
+                    <Route exact path = "/">
+                        <Auth />
+                    </Route>
+                )}
             </Switch>
         </Router>
     )
 }
+
+export default AppRouter;
